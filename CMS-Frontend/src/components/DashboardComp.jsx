1c52244abcf432929b663be41999cf449abcd9eb
abc <abc@abcs-MacBook-Pro.local>
2023-07-06 18:57:24 +0530

    Manage Category Functionalities added

--- CMS-Frontend/src/components/DashboardComp.jsx
+++ CMS-Frontend/src/components/DashboardComp.jsx
@@ -3,32 +3,37 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const DashboardComp = (props) => {
-
-  const {dashboardData} = props
-  console.log("props: ",dashboardData)
+  const { dashboardData } = props;
+  console.log("props: ", dashboardData);
 
   return (
     <div>
       {dashboardData ? (
-        <div style={{display: "flex",  justifyContent:"space-around"}}> 
+        <div style={{ display: "flex", justifyContent: "space-around" }}>
+          <div>
+            <div>Category: {dashboardData.category}</div>
+            <button>
+              <Link to={{ pathname: "/managecategory" }}>View Categories</Link>
+            </button>
+          </div>
           <div>
             <div>Product: {dashboardData.product}</div>
-            <button><Link to={{pathname:"/manageproduct"}}>View Products</Link></button>
+            <button>
+              <Link to={{ pathname: "/manageproduct" }}>View Products</Link>
+            </button>
           </div>
           <div>
             <div>Bill: {dashboardData.bill}</div>
-            <button><Link to={{pathname:"/viewbill"}}>View Products</Link></button>
-          </div>
-          <div>
-            <div>Category: {dashboardData.category}</div>
-            <button><Link to={{pathname:"/managecategory"}}>View Products</Link></button>
+            <button>
+              <Link to={{ pathname: "/viewbill" }}>View Bills</Link>
+            </button>
           </div>
         </div>
       ) : (
         <div>Loading dashboard data...</div>
       )}
     </div>
-      )
-}
+  );
+};
 
-export default DashboardComp
+export default DashboardComp;
--- CMS-Frontend/src/components/ManageCatComp.jsx
+++ CMS-Frontend/src/components/ManageCatComp.jsx
@@ -9,10 +9,66 @@ import {
   CardTitle,
   ListGroup,
   ListGroupItem,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Form,
+  FormGroup,
+  Label,
+  Input,
 } from "reactstrap";
 
-const ManageCatComp = () => {
-  const [categoryDetails, setCategoryDetails] = useState();
+const ManageCatComp = (args) => {
+  const [categoryDetails, setCategoryDetails] = useState([]);
+  const [filteredCategoryDetails, setFilteredCategoryDetails] = useState([]);
+  const [modal, setModal] = useState(false);
+  const [addCat, setAddCat] = useState("");
+  const [addFilter, setAddFilter] = useState("");
+
+  const toggle = () => setModal(!modal);
+
+  let data = JSON.parse(localStorage.getItem("data"));
+  let userToken = data.token;
+
+  const handleFilter = (e) => {
+    setAddFilter(e.target.value);
+    // Filter categoryDetails based on input value
+    const filteredCategories = categoryDetails.filter((category) =>
+      category.name.toLowerCase().includes(e.target.value.toLowerCase())
+    );
+    setFilteredCategoryDetails(filteredCategories);
+  };
+
+  const handleAddCategory = async (e) => {
+    e.preventDefault();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userToken}`,
+      },
+    };
+
+    try {
+      const response = await axios.post(
+        "http://localhost:8081/category/add",
+        {
+          name: addCat,
+        },
+        config
+      );
+
+      toggle();
+
+      console.log("API DATA", response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleChange = (e) => {
+    setAddCat(e.target.value);
+  };
 
   useEffect(() => {
     let data = JSON.parse(localStorage.getItem("data"));
@@ -53,9 +109,12 @@ const ManageCatComp = () => {
           }}
         >
           <CardTitle tag="h5">Manage Category</CardTitle>
-          <Button style={{ background: "#6d7fcc" }}>Add Category</Button>
+          <Button onClick={toggle} style={{ background: "#6d7fcc" }}>
+            Add Category
+          </Button>
         </CardBody>
       </Card>
+
       <Card
         style={{
           width: "95%",
@@ -69,8 +128,10 @@ const ManageCatComp = () => {
           }}
           type="text"
           placeholder="Filter"
+          onChange={handleFilter}
         />
       </Card>
+
       <Card
         style={{
           width: "95%",
@@ -78,12 +139,45 @@ const ManageCatComp = () => {
         }}
       >
         <ListGroup flush>
-          {categoryDetails &&
-            categoryDetails.map((category) => (
-              <ListGroupItem key={category.id}>{category.name}</ListGroupItem>
-            ))}
+          {addFilter
+            ? filteredCategoryDetails.map((category) => (
+                <ListGroupItem key={category.id}>{category.name}</ListGroupItem>
+              ))
+            : categoryDetails.map((category) => (
+                <ListGroupItem key={category.id}>{category.name}</ListGroupItem>
+              ))}
         </ListGroup>
       </Card>
+
+      <Modal isOpen={modal} toggle={toggle} {...args}>
+        <ModalHeader toggle={toggle}>Add Category</ModalHeader>
+        <ModalBody>
+          <Form>
+            <FormGroup>
+              <Input
+                name="category"
+                value={addCat}
+                placeholder="Category Name"
+                type="input"
+                onChange={handleChange}
+                required
+              />
+            </FormGroup>
+          </Form>
+        </ModalBody>
+        <ModalFooter>
+          <Button
+            disabled={!addCat}
+            color="primary"
+            onClick={handleAddCategory}
+          >
+            Add Category
+          </Button>{" "}
+          <Button color="secondary" onClick={toggle}>
+            Cancel
+          </Button>
+        </ModalFooter>
+      </Modal>
     </div>
   );
 };
